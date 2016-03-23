b068570b24c2aaf5374d65d632af68b0062e5732
Mikhail Artemyev <artemyev@tamu.edu>
2016-03-22 20:52:21 -0500

    Found a proper way to output a layer of saturated rock between two homogeneous parts in 3D

--- full_seismic.m
+++ full_seismic.m
@@ -9,8 +9,8 @@ rhoB=(1.0e+3)*rhoB;
 rhoB=reshape(rhoB,60,220,10);
 fclose(fid);
 
-above=(2.2e+3)*ones(60,166,220);
-under=(2.2e+3)*ones(60,84,220);
+above=(2.2e+3)*ones(60,220,86);
+below=(2.2e+3)*ones(60,220,84);
 
 fname='data/rhoB_2_full';
 fid=fopen(fname, 'w');
@@ -18,15 +18,26 @@ if (fid == -1)
     disp('Cannot open file');
     return;
 end
-fwrite(fid,above,'single');
-%fwrite(fid,rhoB,'single');
-for iz=1:size(rhoB,3)
-    for iy=1:size(rhoB,2)
+
+% we take an XY layer and put it as an XZ layer
+
+
+for iy=1:size(rhoB,2)
+    for iz=1:size(above,3)
+       for ix=1:size(above,1)
+           fwrite(fid,above(ix,iy,iz),'single');
+       end
+    end
+    for iz=1:size(rhoB,3)
         for ix=1:size(rhoB,1)
-            fwrite(fid,rhoB(ix,iy,iz),'single');
+            fwrite(fid, rhoB(ix,iy,iz),'single');
         end
     end
+    for iz=1:size(below,3)
+       for ix=1:size(below,1)
+           fwrite(fid,below(ix,iy,iz),'single');
+       end
+    end
 end
-fwrite(fid,under,'single');
 fclose(fid);
 
