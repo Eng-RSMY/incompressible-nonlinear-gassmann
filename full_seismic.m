d9c74ca17ce74ec6c60735bd9625bdd5c2814348
Mikhail Artemyev <artemiev.mikhail@gmail.com>
2016-03-28 09:50:07 -0500

    Add Backus averaging + all old changes with full_seismic and main3D*gassmann

--- full_seismic.m
+++ full_seismic.m
@@ -1,32 +1,41 @@
-fname='data/rhoB_2';
-fid=fopen(fname, 'r');
-if (fid == -1)
-    disp('Cannot open file');
-    return;
-end
-rhoB=fread(fid,[60*220*10 1],'single');
-rhoB=(1.0e+3)*rhoB;
-rhoB=reshape(rhoB,60,220,10);
-fclose(fid);
+dir=sprintf('%s/spe_z85/',getenv('SCRATCH'));
 
-above=(2.2e+3)*ones(60,166,220);
-under=(2.2e+3)*ones(60,84,220);
+fnames={'rhoB_0','vp_0','vs_0'};
+above_prop=[2.2e+3 4.2e+3 2.3e+3];
+below_prop=[2.5e+3 5.9e+3 3.8e+3];
+scale_from_files=1.0e+3;
 
-fname='data/rhoB_2_full';
-fid=fopen(fname, 'w');
-if (fid == -1)
-    disp('Cannot open file');
-    return;
-end
-fwrite(fid,above,'single');
-%fwrite(fid,rhoB,'single');
-for iz=1:size(rhoB,3)
-    for iy=1:size(rhoB,2)
-        for ix=1:size(rhoB,1)
-            fwrite(fid,rhoB(ix,iy,iz),'single');
-        end
+for i=1:size(fnames(:))
+    
+    fname=sprintf('%s/%s', dir, fnames{i});
+    disp(['Input file ', fname]);
+    fid=fopen(fname, 'r');
+    if (fid == -1)
+        disp(['Cannot open file ', fname]);
+        return;
     end
+    arr=fread(fid,[60*220*80 1],'single');
+    arr=scale_from_files*arr;
+    arr=reshape(arr,60,220,80);
+    fclose(fid);
+
+    above=above_prop(i)*ones(60,220,100);
+    below=below_prop(i)*ones(60,220,20);
+
+    fname_full=sprintf('%s_full',fname);
+    disp(['Output file ', fname_full]);
+    fid=fopen(fname_full, 'w');
+    if (fid == -1)
+        disp(['Cannot open file ', fname_full]);
+        return;
+    end
+
+    for iy=1:size(arr,2)
+        fwrite(fid,above(:,iy,:),'single');
+        fwrite(fid,  arr(:,iy,:),'single');
+        fwrite(fid,below(:,iy,:),'single');
+    end
+    fclose(fid);
+
 end
-fwrite(fid,under,'single');
-fclose(fid);
 
